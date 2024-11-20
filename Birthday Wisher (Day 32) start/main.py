0f047c132e5de9a6daa1b4a6b51a64f91bb44efb
adimit078 <toadityamittal@gmail.com>
2024-11-20 02:31:00 -0500

    sent email using smtp

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,1 +1,21 @@
-print("hello")
+import smtplib
+
+from pandas.core.methods.to_dict import to_dict
+
+email = "udemytest555@gmail.com"
+password = "hsff yxll vwjd cbjb"
+
+#Create Connection to gmail server
+with smtplib.SMTP("smtp.gmail.com") as connection:
+
+    #Secure using tls
+    connection.starttls()
+    #username and password
+    connection.login(user=email, password=password)
+
+    #Set up email and close
+    connection.sendmail(from_addr=email, to_addrs="acv49@cornell.edu",
+                        msg=""
+                            "Subject:Hello Drew Visconti @ Bolt Tower"
+                            "\n\nIf you are a handsome 5'Something italian-irish man with a goatee and a single in Bolt Tower - You have been selected - be prepared.")
+
